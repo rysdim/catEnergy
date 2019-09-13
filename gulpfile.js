8c6d8db00dc6762b53c9c7b5614b33e6107c7266
rysdim <dmitry.rysevets@gmail.com>
2019-09-13 14:28:10 +0300

    Добавил зависимости package.json и настроил autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,59 @@
-// function defaultTask(cb) {
-//   // place code for your default task here
-//   cb();
-// }
-//
-// exports.default = defaultTask
-
 "use strict";
 
-var less = require('gulp-less');
-var gulp = require('gulp');
-var path = require('path');
+// var gulp = require('gulp');
+// var less = require('gulp-less');
+// var path = require('path');
+//
+// gulp.task('less', function () {
+//   return gulp.src('./source/less/*.less')
+//     .pipe(less({
+//       paths: [ path.join(__dirname, 'less', 'includes') ]
+//     }))
+//     .pipe(gulp.dest('./source/css'));
+// });
 
-gulp.task('less', function () {
-  return gulp.src('./source/less/*.less')
+var gulp = require("gulp");
+var less = require("gulp-less");
+var path = require("path");
+var plumber = require("gulp-plumber");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var browserSync = require("browser-sync").create();
+
+gulp.task("style", function () {
+  return gulp.src('./source/less/style.less')
+    .pipe(plumber())
     .pipe(less({
       paths: [ path.join(__dirname, 'less', 'includes') ]
     }))
-    .pipe(gulp.dest('./source/css'));
+    .pipe(postcss([autoprefixer()]))
+    .pipe(gulp.dest('./source/css'))
+    .pipe(browserSync.stream());
 });
+
+// gulp.task("serve", ["style"], function() {
+//   browserSync.init({
+//     server: "source/"
+//     // notify: false,
+//     // open: true,
+//     // cors: true,
+//     // ui: false
+//   });
+//
+//   gulp.watch("./source/less/**/*.less", ["style"]);
+//   gulp.watch("*.html").on("change", server.reload);
+// });
+
+gulp.task("serve", function (done) {
+    browserSync.init({
+        server: "source/"
+    });
+    gulp.watch("source/less/**/*.less", gulp.series("style"));
+    gulp.watch("source/*.html").on("change", () => {
+        browserSync.reload();
+        done();
+    });
+
+    done();
+});
+gulp.task("default", gulp.series("style", "serve"));
